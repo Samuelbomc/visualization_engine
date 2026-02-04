1519445f042a819968e384527ee37c426f8cf05c
Samuel Bedoya <samuel.bedoyao@udea.edu.co>
2026-02-04 14:23:42 -0500

    Moved files. Added implementations.cpp to compile the implementation code for single header libraries

---
+++ src/implementations.cpp
@@ -0,0 +1,10 @@
+// This file compiles the implementation code for single-header libraries
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "stb_truetype.h"
